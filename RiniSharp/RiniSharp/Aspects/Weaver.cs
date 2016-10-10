80dbd463b24c0b217c98554c0ac078bc52f51726
pjc02478 <pjc0247@gmail.com>
2016-10-10 18:59:44 +0900

    bye

---
+++ RiniSharp/RiniSharp/Aspects/Weaver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mono.Cecil;
+
+namespace RiniSharp.Aspects 
+{
+    class Weaver
+    {
+        public void AddMethodAspect(Method.IMethodAspect aspect)
+        {
+
+        }
+        public void AddClassAspect(Class.IClassAspect aspect)
+        {
+
+        }
+
+        public void ProcessModule(ModuleDefinition module)
+        {
+
+        }
+    }
+}
