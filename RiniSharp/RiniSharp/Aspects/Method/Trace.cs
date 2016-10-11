6c3c6e2462467bbc69efe6445a3b24f13d4176cf
pjc02478 <pjc0247@gmail.com>
2016-10-11 11:25:07 +0900

    d

---
+++ RiniSharp/RiniSharp/Aspects/Method/Trace.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+using RiniSharpCore;
+using RiniSharpCore.Impl;
+
+using UnityEngine;
+
+namespace RiniSharp.Aspects.Method
+{
+    [AspectTarget(typeof(TraceAttribute))]
+    class Trace : MethodAspect
+    {
+        public override void Apply(MethodDefinition method, CustomAttribute attr)
+        {
+            Console.WriteLine($"   [TRACE] {method.FullName}");
+
+            var beginMethod = typeof(Profiler).GetMethods()
+                .Where(x => x.Name == nameof(Profiler.BeginSample))
+                .Where(x => x.GetParameters().Length == 1)
+                .First();
+            var endMethod = typeof(Profiler).GetMethod(nameof(Profiler.EndSample));
+
+            WvPatterns.WrapMethod.Apply(
+                method,
+                (ilgen, offset) =>
+                {
+                    ilgen.InsertBefore(
+                        offset,
+                        ilgen.Create(OpCodes.Call, Global.module.Import(beginMethod)));
+                    ilgen.InsertBefore(
+                        offset,
+                        ilgen.Create(OpCodes.Ldstr, $"{method.DeclaringType.Name}::{method.Name}()"));
+                },
+                (ilgen, offset) =>
+                {
+                    ilgen.InsertBefore(
+                        offset,
+                        ilgen.Create(OpCodes.Call, Global.module.Import(endMethod)));
+                });
+        }
+    }
+}
--- RiniSharp/RiniSharp/WvPatterns/WrapMethod.cs
+++ RiniSharp/RiniSharp/WvPatterns/WrapMethod.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
 namespace RiniSharp.WvPatterns
 {
     class WrapMethod
     {
+        public static void Apply(MethodDefinition method, WeaveExpr before, WeaveExpr after)
+        {
+            var ilgen = method.Body.GetILProcessor();
+
+            before(ilgen, method.GetHead());
+            after(ilgen, method.GetTail());
+        }
     }
 }
