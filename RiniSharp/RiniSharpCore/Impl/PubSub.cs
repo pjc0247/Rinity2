b11360c3a1d657587c4e9e42318c786bf1cf58cf
pjc02478 <pjc0247@gmail.com>
2016-10-13 14:24:20 +0900

    ㅎ

---
+++ RiniSharp/RiniSharpCore/Impl/PubSub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiniSharpCore.Impl
+{
+    public interface IPubSubMessage { }
+
+    public class NotifyChangeMessage : IPubSubMessage
+    {
+        public string variableName { get; set; }
+        public object newValue { get; set; }
+    }
+
+    public class PubSub
+    {
+        private static Dictionary<string, Action<IPubSubMessage>> handlers { get; set; }
+
+        static PubSub()
+        {
+            handlers = new Dictionary<string, Action<IPubSubMessage>>();
+        }
+
+        public static void Subscribe<T>(string channel, Action<T> handler)
+            where T : IPubSubMessage
+        {
+            if (handlers.ContainsKey(channel) == false)
+                handlers[channel] = (_) => handler((T)_);
+            else
+                handlers[channel] += (_) => handler((T)_);
+        }
+        public static void Publish<T>(string channel, T message)
+            where T : IPubSubMessage
+        {
+            if (handlers.ContainsKey(channel) == false)
+                return;
+
+            handlers[channel].Invoke(message);
+        }
+    }
+}
--- RiniSharp/RiniSharpCore/Impl/SharedVariables.cs
+++ RiniSharp/RiniSharpCore/Impl/SharedVariables.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace RiniSharpCore.Impl
 {
@@ -23,6 +24,30 @@ namespace RiniSharpCore.Impl
         public static void Set<T>(string key, object val)
         {
             pool[key] = val;
+
+            PubSub.Publish($"sharedvar.{key}", new NotifyChangeMessage() {
+                newValue = val,
+                variableName = key
+            });
+        }
+
+        public static string Bind(string str)
+        {
+            var regex = new Regex("\\[\\[([a-zA-Z_0_9@]+)\\]\\]");
+
+            var matches = regex.Matches(str);
+
+            if (matches.Count == 0)
+                return str;
+
+            foreach(Match match in matches)
+            {
+                str = str.Replace(
+                    "[[" + match.Groups[1].Value + "]]",
+                    SharedVariables.Get<object>(match.Groups[1].Value).ToString());
+            }
+
+            return str;
         }
     }
 }
