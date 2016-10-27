8f8ed5adcd36effc9318226684e5640cc6adecb0
brown.j <brown.j@kakaocorp.com>
2016-10-27 17:50:21 +0900

    e

--- RiniSharp/RiniSharp/AutoInection/AutoInjectionRule.cs
+++ RiniSharp/RiniSharp/AutoInection/AutoInjectionRule.cs
@@ -9,4 +9,34 @@ namespace RiniSharp.AutoInection
     class AutoInjectionRule
     {
     }
+
+    class NamespaceUnder : AutoInjectionRule
+    {
+        public string @namespace { get; set; }
+
+        public NamespaceUnder(string @namespace)
+        {
+            this.@namespace = @namespace;
+        }
+    }
+
+    class ClassNamePrefix : AutoInjectionRule
+    {
+        public string prefix { get; set; }
+
+        public ClassNamePrefix(string prefix)
+        {
+            this.prefix = prefix;
+        }
+    }
+
+    class DeriveredFrom : AutoInjectionRule
+    {
+        public string typeName { get; set; }
+
+        public DeriveredFrom(string typeName)
+        {
+            this.typeName = typeName;
+        }
+    }
 }
